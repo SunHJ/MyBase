cc45e7572611e34e6af4eb8f4b003aac43f4c3dd
SunHJ <sun_hj189@163.com>
2014-12-25 12:31:47 +0800

    TrimString Test

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -10,5 +10,11 @@ int main(int argc, char* argv[])
 		lValue = g_InterlockedIncrement(&lValue);
 		printf("%d\n", lValue);
 	}
+	char szStr[] = { 'a', 's', 'd', 'a', 's', 'd', 'c', 'c', 'c', '\0' };
+	PCHAR pcPtr = szStr;
+	printf("%s\n", szStr);
+	g_TrimStingBothEnds(pcPtr, 'c');
+	printf("%s\n", pcPtr);
+	printf("2132");
 	return 0;
 }
